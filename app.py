1ec22cb318df0346eb1c60a0b083592ac302cbaf
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-02-14 16:59:44 -0600

    Updated authentication

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python3
 
+from dotenv import load_dotenv
 import os
 import sys
 import spotipy
 import spotipy.util as util
 from flask import Flask, render_template, url_for, request, redirect
 import random
-#import base64
-
-CLIENT_ID = os.environ["CLIENT_ID"]
-CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-scope = 'user-library-read playlist-modify-public'
+from urllib.parse import quote
+import requests
+import json
 
 app = Flask(__name__)
+
+load_dotenv()
+
+#Client Keys
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+#Spotify URLs
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+#Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 5000
+REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+SCOPE = 'user-library-read playlist-modify-public'
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+global token
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    "client_id": CLIENT_ID
+}
 
 def get_all_song_ids_for_user(username: str, sp):
     playlists = sp.user_playlists(username)
@@ -45,8 +74,16 @@ def is_playlist_created(playlist_name, sp):
     
     return playlist_exists
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route("/")
 def home():
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+
+@app.route("/callback/q", methods = ["GET", "POST"])
+def callback():
+    global token
+
     if request.method == 'POST':
         usernames = []
         try:
@@ -63,22 +100,16 @@ def home():
         except:
             return render_template('spotYourFriends.html')
 
-        
-        image = request.form['image']
-        if not image:
-            image = 'static/hacklahoma.png'
-
         try:
             num_songs = int(request.form['quantity'])
         except:
             return render_template('spotYourFriends.html')
 
-        if len(usernames) >= 2:
-            token = util.prompt_for_user_token(username=usernames[0], scope=scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri='http://127.0.0.1/callback')
+        if len(usernames) >= 1:            
             sp = spotipy.Spotify(auth=token)
 
             shared_songs = get_all_song_ids_for_current_user(sp)
-            for user in usernames[1:]:
+            for user in usernames:
                 try:
                     shared_songs &= get_all_song_ids_for_user(user, sp)
                 except:
@@ -86,7 +117,7 @@ def home():
             
             songs_needed = num_songs - len(shared_songs)
             if songs_needed > 0:
-                all_songs = set()
+                all_songs = get_all_song_ids_for_current_user(sp)
                 for user in usernames:
                     all_songs |= get_all_song_ids_for_user(user, sp)
                 shared_songs |= set(random.sample(all_songs, songs_needed))
@@ -94,19 +125,28 @@ def home():
                 shared_songs = list(shared_songs)[:num_songs]
 
             if not is_playlist_created(playlist_name, sp):
-                playlist_id = sp.user_playlist_create(usernames[0], playlist_name)['id']
-                '''
-                with open(image, 'rb') as image_file:
-                    encoded_string = base64.b64encode(image_file.read())
-                    sp.playlist_upload_cover_image(playlist_id, encoded_string)
-                '''
-                sp.user_playlist_add_tracks(usernames[0], playlist_id, shared_songs)
+                playlist_id = sp.user_playlist_create(sp.me()['id'], playlist_name)['id']
+                sp.user_playlist_add_tracks(sp.me()['id'], playlist_id, shared_songs)
                 return redirect('spotify:playlist:' + playlist_id, code=302)
             else:
                 return render_template('spotYourFriends.html')
     else:
+        auth_token = request.args['code']
+        
+        code_payload = {
+            "grant_type": "authorization_code",
+            "code": str(auth_token),
+            "redirect_uri": REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        }
+
+        post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+        response_data = json.loads(post_request.text)
+        token = response_data["access_token"]
+
         return render_template('spotYourFriends.html')
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=PORT)
